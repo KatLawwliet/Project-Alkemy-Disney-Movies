dc41272d458c48c5b6c4214e3a605ead56c73add
Kat <jime.lawwliet@hotmail.com>
2022-09-02 16:42:35 -0300

    initial commit

---
+++ src/main/java/ProjectAlkemy/Disney/DisneyApplication.java
@@ -0,0 +1,13 @@
+package ProjectAlkemy.Disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProjectAlkemy/Disney/DisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectAlkemy.Disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
